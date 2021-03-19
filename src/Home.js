7feaab85bbff76581585ace4cb6e66f63c6d7675
Warren <tamagrijr@gmail.com>
2021-03-18 19:14:58 -0600

    skeleton complete

--- src/Home.js
+++ src/Home.js
@@ -1,71 +1,298 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import styled from 'styled-components';
 
 export default function Home() {
-
+  
   return (
-    <div class='homePage'>
-
-      <Line class='comment'>
-        <LineNumber>1</LineNumber>
-        &nbsp;
-        {`//Warren Tamagri - Web Developer`}
-      </Line>
-      <Line>
-        <LineNumber>2</LineNumber>
-      </Line>
+    <IntroFunction>
 
       <Line>
-        <LineNumber>3</LineNumber>
+        <Num>1</Num>
         &nbsp;
-        <span class='purp'>{`export default `} </span>
-        <span class='blue'>{`function `} </span>
-        <span class='lightYellow'>{'Hello'}</span>
-        <span class='yellow'>{`() {`}</span>
+        <Comment>{`//Warren Tamagri - Web Developer`}</Comment>
       </Line>
 
-      <Line class='purp'>
-        <span class='lineNumbers'>4</span>
-      &nbsp;&nbsp;&nbsp;
-      {`return (`}
+      <Num>2</Num>
+
+      <Line>
+        <Num>3</Num>
+        &nbsp;
+        <Purp>{`export default `}</Purp>
+        <Blue>{`function `}</Blue>
+        <PaleYellow>{'Hello'}</PaleYellow>
+        <Yellow>{`() {`}</Yellow>
       </Line>
 
-      <div class='line gray'>
-        <span class='lineNumbers'>5</span>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      {`<>`}
-      </div>
+      <Line>
+        <Num>4</Num>
+        &nbsp;&nbsp;&nbsp;
+        <Purp>{`return (`}</Purp>
+      </Line>
 
-      <div class='line green'>
-        <span class='lineNumbers'>6</span>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span class='gray'>{`<`}</span>
-        {`Greeting`}
-        <span class='gray'>{`>`}</span>
-      </div>
 
-      <div class='line'>
-        <span class='lineNumbers'>7</span>
+      <Line>
+        <Num>5</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>6</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Greeting`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>7</Num>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span class='white'>
+        <White>
           <ReactTypingEffect
-            text={['Hello!']}
+            text={['Howdy!']}
             speed={[160]}
             typingDelay={[1500]}
             eraseDelay={[1500]}
           />
-        </span>
-      </div>
+        </White>
+      </Line>
 
-    </div>
+      <Line>
+        <Num>8</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Introduction`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>9</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>{`My name is Warren Tamagri`}</White>
+      </Line>
+
+      <Line>
+        <Num>10</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</`}</Gray>
+        <Green>{`Introduction`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>11</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Bio`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>12</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>{`I am a software engineer`}</White>
+      </Line>
+
+      <Line>
+        <Num>13</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</`}</Gray>
+        <Green>{`Bio`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>14</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</`}</Gray>
+        <Green>{`Greeting`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>15</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Contact`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>16</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>
+          {`email: `}
+          <Email onClick={handleEmail}>
+            {`tamagrijr@gmail.com`}
+          </Email>
+        </White>
+      </Line>
+
+      <Line>
+        <Num>17</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>
+          {`github: `}
+          <Link href="https://github.com/tamagrijr" target="_blank">
+            {`https://github.com/tamagrijr`}
+          </Link>
+        </White>
+      </Line>
+
+      <Line>
+        <Num>18</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>
+          {`linkedin: `}
+          <Link href="https://www.linkedin.com/in/wLinkrren-tamagri-5648a71ba/" target="_blank">
+            {`https://www.linkedin.com/in/warren-tamagri`}
+          </Link>
+        </White>
+      </Line>
+
+      <Line>
+        <Num>19</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</`}</Gray>
+        <Green>{`Contact`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>20</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Appreciation`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>21</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <White>Thanks for checking me out!</White>
+      </Line>
+
+      <Line>
+        <Num>22</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</`}</Gray>
+        <Green>{`Appreciation`}</Green>
+        <Gray>{`>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>23</Num>
+      </Line>
+
+      <Line>
+        <Num>24</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <DarkGreen>{`{/* Click below to see my projects! */}`}</DarkGreen>
+      </Line>
+
+      <Line>
+        <Num>25</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`<`}</Gray>
+        <Green>{`Projects `}</Green>
+        <BlueHover>
+          {`onClick={`}
+          <PaleYellow>{`openProjects`}</PaleYellow>
+          {`}`}
+        </BlueHover>
+        <Gray>{` />`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>26</Num>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Gray>{`</>`}</Gray>
+      </Line>
+
+      <Line>
+        <Num>27</Num>
+        &nbsp;&nbsp;
+        <Purp>{`)`}</Purp>
+      </Line>
+
+      <Line>
+        <Num>28</Num>
+        <Yellow>{`}`}</Yellow>
+      </Line>
+
+      <Line>
+        <Num>29</Num>
+      </Line>
+
+    </IntroFunction>
   )
 }
 
-const LineNumber = styled.span`
-  color: rgb(144, 144, 144);
-  position: relative;
+function handleEmail(){
+  navigator.clipboard.writeText('tamagrijr@gmail.com');
+};
+
+const IntroFunction = styled.div`
+position: relative;
 `;
 const Line = styled.div`
+position: relative;
+`;
+const Num = styled.span`
+  color: rgb(144, 144, 144);
   position: relative;
+  padding-right: .5em;
+`;
+const Comment = styled.span`
+color: white;
+font-size: 2.1vh;
+`;
+const Purp = styled.span`
+  color: rgb(220, 123, 223);
+`;
+const Blue = styled.span`
+  color: rgb(173, 224, 255);
+`;
+const PaleYellow = styled.span`
+  color: rgb(222, 225, 175);
+`;
+const Yellow = styled.span`
+  color: rgb(211, 189, 0);
+`;
+const Gray = styled.span`
+  color: rgb(138, 138, 138);
+`;
+const White = styled.span`
+  color: white;
+`;
+const Green = styled.span`
+  color: rgb(101, 207, 184);
+`;
+const DarkGreen = styled.span`
+  color: rgb(111, 155, 86);
+`;
+const BlueHover = styled.span`
+  color: rgb(173, 224, 255);
+  cursor: pointer;
+
+  :hover {
+    color: rgb(220, 123, 223);
+  }
+`;
+const Link = styled.a`
+  color: white;
+  cursor: pointer;
+
+  :hover{
+    color: rgb(173, 224, 255);
+  }
+`;
+const Email = styled.span`
+  color: white;
+  cursor: pointer;
+
+  :hover{
+    color: rgb(211, 189, 0);
+  }
 `;
